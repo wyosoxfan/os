f3a381106f9477ff357913c0cc3c89cd5a2a72df
Neil Christensen <wyosoxfan@outlook.com>
2020-09-11 07:10:16 -0600

    32 Bit Mode

--- boot.asm
+++ boot.asm
@@ -4,9 +4,7 @@ mov [BOOT_DISK], dl
 mov bp, 0x7c00
 mov sp, bp
 call ReadDisk
-mov bx, PROGRAM_SPACE
-call PrintString
-jmp $
+jmp PROGRAM_SPACE
 %include "video.asm"
 %include "disk.asm"
 times 510-($-$$) db 0
--- kernel.asm
+++ kernel.asm
@@ -1,2 +1,32 @@
 [org 0x7e00]
-db 'This is a test string bitch', 0
+
+jmp EnterProtectedMode
+
+%include "video.asm"
+%include "gdt.asm"
+
+EnterProtectedMode:
+  cli 
+  call EnableA20
+  lgdt [gdt_descriptor]
+  mov eax, cr0
+  or eax, 1
+  mov cr0, eax
+  jmp codeseg:StartProtectedMode
+
+EnableA20:
+  in al, 0x92
+  or al, 2
+  out 0x92, al
+  ret
+
+[bits 32]
+StartProtectedMode:
+  mov ax, dataseg
+  mov ds, ax
+  mov ss, ax
+  mov es, ax
+  mov fs, ax
+  mov gs, ax
+  mov [0xb8000], byte 'N'
+  jmp $
